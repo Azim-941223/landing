8b175b722d83a81410d19d9567bbc1bbff7d5991
Azim-941223 <urmanbetovazim@gmail.com>
2023-09-04 12:30:15 +0600

    feat/animation and bugfix for direction

--- src/components/Direction/Direction.jsx
+++ src/components/Direction/Direction.jsx
@@ -1,54 +1,126 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import scss from "./Direction.module.scss";
+import axios from "axios";
+import { BASE_URL } from "./../constants/constants";
+import { useTranslation } from "react-i18next";
 
 const Direction = () => {
+  const { t } = useTranslation();
+  const [practiceData, setPracticeData] = useState([]);
+  const [selectedPractice, setSelectedPractice] = useState(practiceData[0]);
+  const [selectedTitle, setSelectedTitle] = useState(null); // Define selectedTitle for mobile
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 992);
+
+  const fetchPracticeData = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/api/practice/`);
+      setPracticeData(response.data);
+    } catch (error) {
+      console.error("Error fetching practice data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPracticeData();
+  }, []);
+
+  useEffect(() => {
+    if (practiceData.length > 0) {
+      setSelectedPractice(practiceData[0]);
+    }
+  }, [practiceData]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 992);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handlePracticeSelect = (practice) => {
+    setSelectedPractice(practice);
+  };
+
   return (
     <main>
-      {/* main directions activities: */}
       <div className="container">
         <div className={scss.direction}>
-          <h2>Основные направления деятельности:</h2>
+          <h2>{t("directions.direc")}</h2>
           <div className={scss.direction_block}>
-            <div className={scss.direction_desc}>
-              <div className={scss.direction_desc_title}>
-                <h4>Разработка программного обеспечения</h4>
-              </div>
-              <div className={scss.direction_desc_title}>
-                <h4>Автоматизация бизнес-процессов</h4>
-              </div>
-              <div className={scss.direction_desc_title}>
-                <h4>Разработка технического задания</h4>
-              </div>
-            </div>
-            <div className={scss.direction_desc_about}>
-              <div className={scss.direction_desc_description}>
-                <p className={scss.desc}>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Perferendis doloribus nihil porro culpa rerum praesentium
-                  inventore doloremque nam, maxime eius aut unde natus deleniti
-                  itaque commodi, non veritatis minima ratione dolor qui quas
-                  officiis accusantium suscipit ad! In repellat architecto,
-                  officiis ullam magnam consequuntur. Labore temporibus sunt
-                  quas dolore iure? Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Perferendis doloribus nihil porro culpa
-                  rerum praesentium inventore doloremque nam, maxime eius aut
-                  unde natus deleniti itaque commodi, non veritatis minima
-                  ratione dolor qui quas officiis accusantium suscipit ad! In
-                  repellat architecto, officiis ullam magnam consequuntur.
-                  Labore temporibus sunt quas dolore iure?
-                </p>
-              </div>
-              <div className={scss.direction_desc_images}>
-                <img
-                  src="https://www.croc.ru/wp-content/uploads/2023/05/main-competence_1.jpg"
-                  alt=""
-                />
-              </div>
-            </div>
+            {isDesktop ? (
+              // Desktop version
+              <>
+                <div className={scss.direction_desc}>
+                  {practiceData.map((item) => (
+                    <div
+                      key={item.title}
+                      onClick={() => handlePracticeSelect(item)}
+                      className={
+                        selectedPractice === item
+                          ? scss.direction_desc_title_active
+                          : scss.direction_desc_title
+                      }
+                    >
+                      <h4>{item.title}</h4>
+                    </div>
+                  ))}
+                </div>
+                <div className={scss.direction_desc_about}>
+                  <div className={scss.direction_desc_description}>
+                    {selectedPractice && (
+                      <p className={scss.desc}>
+                        {selectedPractice.description}
+                      </p>
+                    )}
+                  </div>
+                  <div className={scss.direction_desc_images}>
+                    {selectedPractice && (
+                      <img src={selectedPractice.image} alt="" />
+                    )}
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                {practiceData.map((item) => (
+                  <div key={item.title} className={scss.direction_desc_mobile}>
+                    <div
+                      onClick={() =>
+                        setSelectedTitle(
+                          selectedTitle === item.title ? null : item.title
+                        )
+                      }
+                      className={
+                        selectedTitle === item.title
+                          ? `${scss.direction_desc_title} ${scss.direction_desc_title_active}`
+                          : scss.direction_desc_title
+                      }
+                    >
+                      <h4>{item.title}</h4>
+                      <span className={scss.arrow}></span>
+                    </div>
+                    {selectedTitle === item.title && (
+                      <div className={scss.direction_desc_about}>
+                        <div className={scss.direction_desc_description}>
+                          <p className={scss.desc}>{item.description}</p>
+                        </div>
+                        <div className={scss.direction_desc_images}>
+                          <img src={item.image} alt="" />
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </>
+            )}
           </div>
         </div>
       </div>
-      {/* main directions activities end */}
     </main>
   );
 };
